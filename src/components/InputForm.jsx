b7dc20d0ca0c90078b17494c70221b62a9ce502c
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-17 14:21:28 -0500

    set up the state variables and the on change event handlers for the form

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function InputForm() {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [pages, setPages] = useState('')
+  const [have_read, setHave_read] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
     <>
       <section>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h3>Input Book Details</h3>
-          <label for="title">
+          <label htmlFor="title">
             Book Title:
             <br />
-            <input type="text" id="title"></input>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            ></input>
           </label>
 
-          <label for="author">
+          <label htmlFor="author">
             Book Author:
             <br />
-            <input type="text" id="author"></input>
+            <input
+              type="text"
+              id="author"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            ></input>
           </label>
 
-          <label for="pages">
-            Number of Pages: <input type="text" id="pages"></input>
+          <label htmlFor="pages">
+            Number of Pages:{' '}
+            <input
+              type="text"
+              id="pages"
+              value={pages}
+              onChange={(e) => setPages(e.target.value)}
+            ></input>
           </label>
 
-          <label for="have_read">
-            Have you read this book: <input type="text" id="have_read"></input>
+          <label htmlFor="have_read">
+            Have you read this book:{' '}
+            <input
+              type="text"
+              id="have_read"
+              value={have_read}
+              onChange={(e) => setHave_read(e.target.value)}
+            ></input>
           </label>
 
           <button type="submit">Submit</button>
