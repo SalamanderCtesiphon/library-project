6780a61f996b2c0c191921fcda78ec82d1028f1e
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-17 17:31:45 -0500

    implemented uuid for creating uniqued id's for new books that are submited

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import uuid from 'react-uuid'
 import Header from './components/Header'
 import BookForm from './components/BookForm'
 import BookCard from './components/BookCard'
@@ -10,20 +11,24 @@ function App() {
   const [pages, setPages] = useState('')
   const [have_read, setHave_read] = useState('')
   const [book, setBook] = useState(null)
+  const [id, setId] = useState(uuid())
   const [myLibrary, setMyLibrary] = useState([
     {
+      id: '1',
       title: 'Crime and Punishment',
       author: 'Fyodor Dostoevsky',
       pages: '542',
       have_read: 'yes',
     },
     {
+      id: '2',
       title: 'The Hobbit',
       author: 'J. R. R. Tolkien',
       pages: '305',
       have_read: 'yes',
     },
     {
+      id: '3',
       title: 'Don Quixote',
       author: 'Miguel DeCervantes',
       pages: '1034',
@@ -33,18 +38,24 @@ function App() {
 
   function handleSubmit(e) {
     e.preventDefault()
+    setId(uuid())
     setBook({
+      id,
       title,
       author,
       pages,
       have_read,
     })
-    setMyLibrary([...myLibrary, { title, author, pages, have_read }])
+    setMyLibrary([...myLibrary, { id, title, author, pages, have_read }])
     setTitle('')
     setAuthor('')
     setPages('')
     setHave_read('')
   }
+
+  useEffect(() => {
+    console.log(myLibrary)
+  }, [myLibrary])
 
   return (
     <>
@@ -62,7 +73,7 @@ function App() {
       />
       <section className="library-display">
         {myLibrary.map((item) => (
-          <BookCard item={item} key={item.title} />
+          <BookCard item={item} key={item.id} />
         ))}
       </section>
     </>
