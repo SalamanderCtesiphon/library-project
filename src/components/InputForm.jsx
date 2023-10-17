50ca0c4d52fb3beb562875c7aaa3cbf223883950
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-17 13:29:38 -0500

    added  a prettier config file and added a book form

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -6,6 +6,14 @@ function InputForm() {
       <section>
         <form>
           <h3>Input Book Details</h3>
+          <label for="title">Title:</label>
+          <input type="text" id="title"></input>
+          <label for="author">Author:</label>
+          <input type="text" id="author"></input>
+          <label for="pages">Number of Pages:</label>
+          <input type="text" id="pages"></input>
+          <label for="have_read">Have you read this book:</label>
+          <input type="text" id="have_read"></input>
         </form>
       </section>
     </>
