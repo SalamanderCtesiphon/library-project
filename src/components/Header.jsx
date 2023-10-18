4cc2e56e65345955ca9c839d7f8493bffec279e1
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-17 20:46:16 -0500

    implemented a basic modal from material ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FormDialog from './FormDialog'
 
 function Header() {
   return (
     <>
       <header className="header">
         <h1>The Library</h1>
+        <FormDialog />
       </header>
     </>
   )
